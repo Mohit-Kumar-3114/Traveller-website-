9d1b9cce5557ca1b9cddba909f8f5e7c3b7d64b0
unknown <mohitahlawat912@gmail.com>
2025-03-24 12:22:58 +0530

    project

---
+++ travel/app/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/navbar";
+import C1 from "@/components/c1";
+import C2 from "@/components/c2";
+import C3 from "@/components/c3";
+export default function Home() {
+  return (
+   <>
+       <Navbar/>
+       <C1/>
+       <C2/>
+       <C3/>
+   </>
+  );
+}
---
+++ travel/components/navbar.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between p-4 bg-white border-b border-gray-200 h-24">
+      <div className="text-4xl px-10">
+        <span className=" text-sky-950">HIMALE</span>
+        <span className="text-sky-400">∩</span>
+        <span className=" text-sky-950">S</span>
+      </div>
+
+     
+      <div className="hidden md:flex space-x-8 font-sans text-lg  text-sky-950  px-10">
+        <a href="" className="hover:text-sky-400 transition">Tours</a>
+        <a href="#c2" className="hover:text-sky-400 transition">Personalized Travel</a>
+        <a href="" className="hover:text-sky-400 transition">Our Story</a>
+        <a href="" className="hover:text-sky-400 transition">Contact Us</a>
+      </div>
+    </nav>
+  );
+}
