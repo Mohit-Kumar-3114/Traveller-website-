162b87fd253be798c037583a1c9138b957548e6f
Mohit Kumar <140935626+Mohit-Kumar-3114@users.noreply.github.com>
2025-03-29 12:43:39 +0530

    Update c2.tsx

--- travel/components/c2.tsx
+++ travel/components/c2.tsx
@@ -4,7 +4,7 @@ export default function C2() {
     return (
         
       <div className="text-center px-4 mt-20" id="c2">
-        <h3 className="text-2xl font-semibold  text-sky-950  ">
+        <h3 className="text-2xl font-semibold  text-sky-950 ">
           <span className=" text-sky-950 ">Personalised Travel</span> <span>by HIMALE<span className="text-sky-400">∩</span>S</span>
         </h3>
         <h2 className="text-4xl font-bold text-sky-950 mt-1">
